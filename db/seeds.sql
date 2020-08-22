c2636458c194057ab0d977e51e95f4d144b80bed
Kim Credit <kim@kimcredit.com>
2020-08-22 17:04:50 -0400

    added db seeds to snacks table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+snacks (category, snack, devoured) 
+
+VALUES 
+('healthy', 'carrots & hummus', false),
+('junk', 'cheetos', false),
+('junk', 'doritos', false),
+('healthy', 'edamame', false),
+('junk', 'oreos', false);
